28f71b5d91c7c1fba248509d11941febef8f9560
ellen.kuo <ellen.kuo@patrioimemory.com>
2026-02-24 15:20:03 +0800

    feat: add Reverse Bits

---
+++ solutions/190. Reverse Bits/Reverse Bits.js
@@ -0,0 +1,7 @@
+/*
+var reverseBits = function(n) {
+  let str = n.toString(2).padStart(32, "0").split("").reverse().join("")
+  return parseInt(str, 2)
+};
+*/
+console.log(reverseBits(43261596)) // 00000010100101000001111010011100
