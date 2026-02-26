c1be105bd3c49efe8ea230960c2dee0dbc3fcacd
ellen.kuo <ellen.kuo@patrioimemory.com>
2026-02-26 16:43:32 +0800

    feat: Add Palindrome Linked List

---
+++ solutions/234. Palindrome Linked List/Palindrome Linked List.js
@@ -0,0 +1,26 @@
+var isPalindrome = function(head) {
+  let fast = head
+  let slow = head
+  while(fast.next !== null || fast.next.next !== null ){
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  let cur = slow.next
+  let prev = null
+  while(cur !== null) {
+    let temp = cur.next
+    cur.next = prev
+    prev = cur
+    cur = temp
+  }
+
+  let p1 = head
+  let p2 = prev
+  while(p2 !== null) {
+    if(p2.val !== p1.val) return false
+    p1 = p1.next
+    p2 = p2.next
+  }
+  return true
+};
